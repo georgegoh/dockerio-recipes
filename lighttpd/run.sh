06b0ac4c65ebbb6587f59463a1fadcc27f06d796
George Goh <georgegohkokleong@gmail.com>
2013-10-07 17:22:05 +0800

    Add containers for tinyproxy and lighttpd.

---
+++ lighttpd/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+usage ()
+{
+  echo "Run with root permissions and supply the target port you want to expose to."
+  echo
+  echo "  Examples: "
+  echo "    $0" "<port>"
+  echo "    sudo" "$0" "<port>"
+  echo
+}
+
+port=$1
+
+if test -z "$port"
+then
+  usage
+  exit 1
+fi
+
+if [[ ! "$port" =~ ^[0-9]+$ ]] || [ "$port" -gt "65535" ] || [ "$port" -lt "1024" ]  
+then
+  echo "Please use a port number between 1024-65535"
+  echo
+  exit 1
+fi
+
+echo "$port"
+docker run -p "$port":80 -d lighttpd
